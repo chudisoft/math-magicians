87a23a556056e714bf43abded364155e864d8ae1
christopheruduekwe@gmail.com <christopheruduekwe@gmail.com>
2023-08-09 14:02:36 +0100

    Fix stylint and eslint errors.

--- src/components/Calculator/Calculator.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Calculator from './Calculator';
-
-it('It should mount', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Calculator />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
