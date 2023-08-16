4576fbbcaa0bf3dde6e0a01c08d5d636e03536d2
Tanmayi Manku <tanmayitanum@gmail.com>
2023-08-16 14:08:37 +0530

    Add test case for operate and calculate test files and fix eslint errors

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -1,11 +1,25 @@
 import operate from './operate';
 
-test('Test Operate', () => {
-  let calculatorData = {
-    total: null,
-    next: null,
-    operation: null,
-  };
-  calculatorData = operate(1, 2, '+');
-  expect(calculatorData).toBe('3');
+describe('Test cases for operations', () => {
+  test('Case 1: Addition', () => {
+    let calculatorData = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    calculatorData = operate(1, 2, '+');
+    expect(calculatorData).toBe('3');
+  });
+
+  test('Case 2: Subtraction', () => {
+    expect(operate('10', '5', '-')).toBe('5');
+  });
+
+  test('Case 3: Number divide by 0', () => {
+    expect(operate('1', '0', '÷')).toBe("Can't divide by 0.");
+  });
+
+  test('Case 4: number divide by modulus', () => {
+    expect(operate('1', '0', '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
 });
